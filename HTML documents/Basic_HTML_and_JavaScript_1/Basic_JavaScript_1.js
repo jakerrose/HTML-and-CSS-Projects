23aa3b0925afa2c0ef7ed53f6694f9b5523a7a3f
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-04-28 09:39:09 -0700

    BASIC HTML AND JAVASCRIPT 1 SUBMISSION ASSIGNMENT

--- HTML documents/Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ HTML documents/Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,3 +1,6 @@
-<script>
-alert("Stop using the internet!");
-</script>
+function My_First_Function() {
+var String="Kiss me, I'm Irish!";
+var result=String.fontcolor("green");
+document.getElementById("Irish").innerHTML=
+result;
+}
